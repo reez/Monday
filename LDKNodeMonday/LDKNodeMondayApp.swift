275b016ca32ce9e4dcc1f77262ae9288d88f2e9f
Matthew <6657488+reez@users.noreply.github.com>
2023-06-03 12:31:13 -0500

    misc: formatting

--- LDKNodeMonday/LDKNodeMondayApp.swift
+++ LDKNodeMonday/LDKNodeMondayApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct LDKNodeMondayApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
+    
     var body: some Scene {
         WindowGroup {
             StartView(viewModel: .init())
