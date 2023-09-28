0e9d8c49bec0a5608545113a56ff63f47157b4f1
Matthew <ramsden@hey.com>
2023-09-28 10:31:57 -0500

    remove mock

--- LDKNodeMonday/KeyService.swift
+++ LDKNodeMonday/KeyService.swift
@@ -63,38 +63,3 @@ extension KeyClient {
         deleteBackupInfo: { try KeyService().deleteBackupInfo() }
     )
 }
-
-//#if DEBUG
-//    extension KeyClient {
-//        static let mock = Self(
-//            saveBackupInfo: { _ in },
-//            getBackupInfo: {
-//                let mnemonicWords12 =
-//                    "space echo position wrist orient erupt relief museum myself grain wisdom tumble"
-//                let mnemonic = try Mnemonic.fromString(mnemonic: mnemonicWords12)
-//                let secretKey = DescriptorSecretKey(
-//                    network: mockKeyClientNetwork,
-//                    mnemonic: mnemonic,
-//                    password: nil
-//                )
-//                let descriptor = Descriptor.newBip86(
-//                    secretKey: secretKey,
-//                    keychain: .external,
-//                    network: mockKeyClientNetwork
-//                )
-//                let changeDescriptor = Descriptor.newBip86(
-//                    secretKey: secretKey,
-//                    keychain: .internal,
-//                    network: mockKeyClientNetwork
-//                )
-//                let backupInfo = BackupInfo(
-//                    mnemonic: mnemonic.asString(),
-//                    descriptor: descriptor.asString(),
-//                    changeDescriptor: changeDescriptor.asStringPrivate()
-//                )
-//                return backupInfo
-//            },
-//            deleteBackupInfo: { try KeyService().deleteBackupInfo() }
-//        )
-//    }
-//#endif
