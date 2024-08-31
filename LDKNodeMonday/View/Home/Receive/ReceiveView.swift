84a6c6d0f9cecd9d70555eaf9e5db1431fc5fd2f
Matthew <ramsden@hey.com>
2024-08-31 14:44:20 -0500

    ui: receive bip21view

--- LDKNodeMonday/View/Home/Receive/ReceiveView.swift
+++ LDKNodeMonday/View/Home/Receive/ReceiveView.swift
@@ -5,6 +5,7 @@
 //  Created by Matthew Ramsden on 2/25/24.
 //
 
+import BitcoinUI
 import SwiftUI
 
 struct ReceiveView: View {
@@ -40,6 +41,51 @@ struct ReceiveView: View {
 
     }
 
+}
+
+struct AmountEntryView: View {
+    @Binding var amount: String
+    @Environment(\.dismiss) private var dismiss
+
+    @State private var numpadAmount = "0"
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("\(numpadAmount.formattedAmount(defaultValue: "0")) sats")
+                .textStyle(BitcoinTitle1())
+                .padding()
+
+            GeometryReader { geometry in
+                let buttonSize = geometry.size.width / 4
+                VStack(spacing: buttonSize / 10) {
+                    numpadRow(["1", "2", "3"], buttonSize: buttonSize)
+                    numpadRow(["4", "5", "6"], buttonSize: buttonSize)
+                    numpadRow(["7", "8", "9"], buttonSize: buttonSize)
+                    numpadRow([" ", "0", "<"], buttonSize: buttonSize)
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .frame(height: 300)
+
+            Button("Confirm") {
+                amount = numpadAmount
+                dismiss()
+            }
+            .padding()
+            .buttonBorderShape(.capsule)
+            .buttonStyle(.bordered)
+        }
+        .padding()
+    }
+
+    func numpadRow(_ characters: [String], buttonSize: CGFloat) -> some View {
+        HStack(spacing: buttonSize / 2) {
+            ForEach(characters, id: \.self) { character in
+                NumpadButton(numpadAmount: $numpadAmount, character: character)
+                    .frame(width: buttonSize, height: buttonSize / 1.5)
+            }
+        }
+    }
 }
 
 #Preview {
