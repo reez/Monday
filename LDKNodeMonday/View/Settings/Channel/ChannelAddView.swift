5412f884ef409741b024b5a8fa6279afe152a5d6
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2024-12-12 13:50:03 +0000

    Swift format

--- LDKNodeMonday/View/Settings/Channel/ChannelAddView.swift
+++ LDKNodeMonday/View/Settings/Channel/ChannelAddView.swift
@@ -20,190 +20,195 @@ struct ChannelAddView: View {
 
     var body: some View {
 
-            VStack {
+        VStack {
 
-                if viewModel.isProgressViewShowing {
-                    HStack {
-                        Spacer()
-                        ProgressView()
-                        Spacer()
+            if viewModel.isProgressViewShowing {
+                HStack {
+                    Spacer()
+                    ProgressView()
+                    Spacer()
+                }
+            }
+
+            Spacer()
+
+            Text(
+                "Enter, paste or scan the required information to open a channel with another lightning node."
+            )
+            .font(.body)
+            .multilineTextAlignment(.center)
+            .fixedSize(horizontal: false, vertical: false)
+            .padding(.horizontal)
+
+            Spacer()
+
+            VStack(spacing: 20) {
+
+                VStack(alignment: .leading) {
+                    Text("Node ID")
+                        .font(.subheadline.weight(.medium))
+                    TextField(
+                        "03a5b467d7f...4c2b099b8250c",
+                        text: $viewModel.nodeId
+                    )
+                    .frame(width: 260, height: 48)
+                    .tint(.accentColor)
+                    .padding([.leading, .trailing], 20)
+                    .keyboardType(.numbersAndPunctuation)
+                    .truncationMode(.middle)
+                    .submitLabel(.next)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 24)
+                            .stroke(Color.accentColor, lineWidth: 2)
+                    ).onChange(of: viewModel.nodeId) { oldValue, newValue in
+                        viewModel.nodeId = newValue.replacingOccurrences(of: " ", with: "")
                     }
                 }
-                
-                Spacer()
-                
-               Text("Enter, paste or scan the required information to open a channel with another lightning node.")
-                    .font(.body)
-                    .multilineTextAlignment(.center)
-                    .fixedSize(horizontal: false, vertical: false)
-                    .padding(.horizontal)
-                    
-                Spacer()
 
-                VStack(spacing: 20) {
-
-                    VStack(alignment: .leading) {
-                        Text("Node ID")
-                            .font(.subheadline.weight(.medium))
-                        TextField(
-                            "03a5b467d7f...4c2b099b8250c",
-                            text: $viewModel.nodeId
-                        )
-                        .frame(width: 260, height: 48)
-                        .tint(.accentColor)
-                        .padding([.leading, .trailing], 20)
-                        .keyboardType(.numbersAndPunctuation)
-                        .truncationMode(.middle)
-                        .submitLabel(.next)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 24)
-                                .stroke(Color.accentColor, lineWidth: 2)
-                        ).onChange(of: viewModel.nodeId) { oldValue, newValue in
-                            viewModel.nodeId = newValue.replacingOccurrences(of: " ", with: "")
-                        }
+                VStack(alignment: .leading) {
+                    Text("Address")
+                        .font(.subheadline.weight(.medium))
+                    TextField(
+                        "172.18.0.2:9735",
+                        text: $viewModel.address
+                    )
+                    .frame(width: 260, height: 48)
+                    .tint(.accentColor)
+                    .padding([.leading, .trailing], 20)
+                    .keyboardType(.numbersAndPunctuation)
+                    .truncationMode(.middle)
+                    .submitLabel(.next)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 24)
+                            .stroke(Color.accentColor, lineWidth: 2)
+                    ).onChange(of: viewModel.address) { oldValue, newValue in
+                        viewModel.address = newValue.replacingOccurrences(of: " ", with: "")
                     }
-                    
-                    VStack(alignment: .leading) {
-                        Text("Address")
-                            .font(.subheadline.weight(.medium))
-                        TextField(
-                            "172.18.0.2:9735",
-                            text: $viewModel.address
-                        )
-                        .frame(width: 260, height: 48)
-                        .tint(.accentColor)
-                        .padding([.leading, .trailing], 20)
-                        .keyboardType(.numbersAndPunctuation)
-                        .truncationMode(.middle)
-                        .submitLabel(.next)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 24)
-                                .stroke(Color.accentColor, lineWidth: 2)
-                        ).onChange(of: viewModel.address) { oldValue, newValue in
-                            viewModel.address = newValue.replacingOccurrences(of: " ", with: "")
-                        }
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        Text("Channel capacity")
-                            .font(.subheadline.weight(.medium))
-                        TextField(
-                            125000.formatted(),
-                            text: $viewModel.channelAmountSats
-                        )
-                        .frame(width: 260, height: 48)
-                        .tint(.accentColor)
-                        .padding([.leading, .trailing], 20)
-                        .keyboardType(.numberPad)
-                        .submitLabel(.done)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 24)
-                                .stroke(Color.accentColor, lineWidth: 2)
-                        )
-                    }
-
                 }
-                .padding(.horizontal)
-                
-                Spacer()
-                
-                HStack(spacing: 60) {
 
-                    Button("Paste", systemImage: "doc.on.clipboard") {
-                        if pasteboard.hasStrings {
-                            if let string = pasteboard.string {
-                                if let peer = string.parseConnectionInfo() {
-                                    viewModel.nodeId = peer.nodeID
-                                    viewModel.address = peer.address
-                                } else {
-                                    viewModel.channelAddViewError = .init(
-                                        title: "Unexpected error",
-                                        detail: "Connection info could not be parsed."
-                                    )
-                                }
+                VStack(alignment: .leading) {
+                    Text("Channel capacity")
+                        .font(.subheadline.weight(.medium))
+                    TextField(
+                        125000.formatted(),
+                        text: $viewModel.channelAmountSats
+                    )
+                    .frame(width: 260, height: 48)
+                    .tint(.accentColor)
+                    .padding([.leading, .trailing], 20)
+                    .keyboardType(.numberPad)
+                    .submitLabel(.done)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 24)
+                            .stroke(Color.accentColor, lineWidth: 2)
+                    )
+                }
+
+            }
+            .padding(.horizontal)
+
+            Spacer()
+
+            HStack(spacing: 60) {
+
+                Button("Paste", systemImage: "doc.on.clipboard") {
+                    if pasteboard.hasStrings {
+                        if let string = pasteboard.string {
+                            if let peer = string.parseConnectionInfo() {
+                                viewModel.nodeId = peer.nodeID
+                                viewModel.address = peer.address
                             } else {
-                                DispatchQueue.main.async {
-                                    viewModel.channelAddViewError = .init(
-                                        title: "Unexpected error",
-                                        detail: "Failed to retrieve string from pasteboard."
-                                    )
-                                }
+                                viewModel.channelAddViewError = .init(
+                                    title: "Unexpected error",
+                                    detail: "Connection info could not be parsed."
+                                )
                             }
                         } else {
                             DispatchQueue.main.async {
                                 viewModel.channelAddViewError = .init(
                                     title: "Unexpected error",
-                                    detail: "Pasteboard has no text."
+                                    detail: "Failed to retrieve string from pasteboard."
                                 )
                             }
                         }
-                    }
-                    
-                    Button("Scan", systemImage: "qrcode.viewfinder") {
-                        isShowingScanner = true
-                    }
-
-                }
-                .buttonStyle(.automatic)
-                .controlSize(.mini)
-                
-                Spacer()
-
-                Button("Open Channel") {
-                    isFocused = false
-                    let channelAmountSats = UInt64(viewModel.channelAmountSats) ?? UInt64(101010)
-                    Task {
-                        await viewModel.openChannel(
-                            nodeId: viewModel.nodeId,
-                            address: viewModel.address,
-                            channelAmountSats: channelAmountSats,
-                            pushToCounterpartyMsat: nil
-                        )
-                        if viewModel.isOpenChannelFinished == true {
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-                                self.presentationMode.wrappedValue.dismiss()
-                            }
+                    } else {
+                        DispatchQueue.main.async {
+                            viewModel.channelAddViewError = .init(
+                                title: "Unexpected error",
+                                detail: "Pasteboard has no text."
+                            )
                         }
                     }
                 }
-                .disabled(!viewModel.nodeId.isEmpty || !viewModel.address.isEmpty || !viewModel.channelAmountSats.isEmpty)
-                .buttonStyle(BitcoinFilled(tintColor: .accentColor , isCapsule: true))
-                    .padding(.horizontal)
-                    .padding(.bottom, 40.0)
+
+                Button("Scan", systemImage: "qrcode.viewfinder") {
+                    isShowingScanner = true
+                }
 
             }
-            .listStyle(.plain)
-            .scrollContentBackground(.hidden)
-            .navigationTitle("Add Channel")
-            .navigationBarTitleDisplayMode(.inline)
-            .padding()
-            .focused($isFocused)
-            .onAppear {
-                viewModel.getColor()
-            }
-            .onReceive(viewModel.$channelAddViewError) { errorMessage in
-                if errorMessage != nil {
-                    showingChannelAddViewErrorAlert = true
+            .buttonStyle(.automatic)
+            .controlSize(.mini)
+
+            Spacer()
+
+            Button("Open Channel") {
+                isFocused = false
+                let channelAmountSats = UInt64(viewModel.channelAmountSats) ?? UInt64(101010)
+                Task {
+                    await viewModel.openChannel(
+                        nodeId: viewModel.nodeId,
+                        address: viewModel.address,
+                        channelAmountSats: channelAmountSats,
+                        pushToCounterpartyMsat: nil
+                    )
+                    if viewModel.isOpenChannelFinished == true {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                    }
                 }
             }
-            .onReceive(viewModel.$isOpenChannelFinished) { _ in }
-            .alert(isPresented: $showingChannelAddViewErrorAlert) {
-                Alert(
-                    title: Text(viewModel.channelAddViewError?.title ?? "Unknown"),
-                    message: Text(viewModel.channelAddViewError?.detail ?? ""),
-                    dismissButton: .default(Text("OK")) {
-                        viewModel.channelAddViewError = nil
-                    }
-                )
+            .disabled(
+                !viewModel.nodeId.isEmpty || !viewModel.address.isEmpty
+                    || !viewModel.channelAmountSats.isEmpty
+            )
+            .buttonStyle(BitcoinFilled(tintColor: .accentColor, isCapsule: true))
+            .padding(.horizontal)
+            .padding(.bottom, 40.0)
+
+        }
+        .listStyle(.plain)
+        .scrollContentBackground(.hidden)
+        .navigationTitle("Add Channel")
+        .navigationBarTitleDisplayMode(.inline)
+        .padding()
+        .focused($isFocused)
+        .onAppear {
+            viewModel.getColor()
+        }
+        .onReceive(viewModel.$channelAddViewError) { errorMessage in
+            if errorMessage != nil {
+                showingChannelAddViewErrorAlert = true
             }
-            .sheet(isPresented: $isShowingScanner) {
-                CodeScannerView(
-                    codeTypes: [.qr],
-                    simulatedData:
-                        "LNBC10U1P3PJ257PP5YZTKWJCZ5FTL5LAXKAV23ZMZEKAW37ZK6KMV80PK4XAEV5QHTZ7QDPDWD3XGER9WD5KWM36YPRX7U3QD36KUCMGYP282ETNV3SHJCQZPGXQYZ5VQSP5USYC4LK9CHSFP53KVCNVQ456GANH60D89REYKDNGSMTJ6YW3NHVQ9QYYSSQJCEWM5CJWZ4A6RFJX77C490YCED6PEMK0UPKXHY89CMM7SCT66K8GNEANWYKZGDRWRFJE69H9U5U0W57RRCSYSAS7GADWMZXC8C6T0SPJAZUP6",
-                    completion: handleScan
-                )
-            }
+        }
+        .onReceive(viewModel.$isOpenChannelFinished) { _ in }
+        .alert(isPresented: $showingChannelAddViewErrorAlert) {
+            Alert(
+                title: Text(viewModel.channelAddViewError?.title ?? "Unknown"),
+                message: Text(viewModel.channelAddViewError?.detail ?? ""),
+                dismissButton: .default(Text("OK")) {
+                    viewModel.channelAddViewError = nil
+                }
+            )
+        }
+        .sheet(isPresented: $isShowingScanner) {
+            CodeScannerView(
+                codeTypes: [.qr],
+                simulatedData:
+                    "LNBC10U1P3PJ257PP5YZTKWJCZ5FTL5LAXKAV23ZMZEKAW37ZK6KMV80PK4XAEV5QHTZ7QDPDWD3XGER9WD5KWM36YPRX7U3QD36KUCMGYP282ETNV3SHJCQZPGXQYZ5VQSP5USYC4LK9CHSFP53KVCNVQ456GANH60D89REYKDNGSMTJ6YW3NHVQ9QYYSSQJCEWM5CJWZ4A6RFJX77C490YCED6PEMK0UPKXHY89CMM7SCT66K8GNEANWYKZGDRWRFJE69H9U5U0W57RRCSYSAS7GADWMZXC8C6T0SPJAZUP6",
+                completion: handleScan
+            )
+        }
 
     }
 
