1a0ac80ea8e320a6d41202073f3b8859552d69a0
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2024-12-12 09:44:56 +0000

    WIP restyle settings view, new sections

--- LDKNodeMonday/View/Settings/SettingsView.swift
+++ LDKNodeMonday/View/Settings/SettingsView.swift
@@ -6,8 +6,8 @@
 //
 
 import BitcoinUI
-import SwiftUI
 import LDKNode
+import SwiftUI
 
 struct SettingsView: View {
     @ObservedObject var viewModel: NodeIDViewModel
@@ -27,58 +27,77 @@ struct SettingsView: View {
     var body: some View {
 
         NavigationView {
+            Form {
 
-                List {
-
-                    Section(
-                        header: Text("Lightning node details")
-                    ) {
-                        HStack {
-                            Text("Network")
-                            Spacer()
-                            Text((viewModel.network ?? "No network").capitalized)
-                        }
-                        
-                        HStack {
-                            Text("Node Id")
-                            Spacer()
-                            Text(viewModel.nodeID)
-                                .frame(width: 200)
-                                .truncationMode(.middle)
-                                .lineLimit(1)
-                            Button {
-                                UIPasteboard.general.string = viewModel.nodeID
-                            } label: {
-                                Image(systemName: "doc.on.doc")
-                            }
-                        }
-                        
-                        HStack {
-                            Text("Status")
-                            Spacer()
-                            HStack {
-                                Text(viewModel.status?.isRunning ?? false ? "On" : "Off")
-                                Image(systemName: "circle.fill")
-                                    .foregroundColor(
-                                        viewModel.status?.isRunning ?? false ? .green : .red
-                                    )
-                            }
-                        }
-                        
-                        NavigationLink("Channels") {
-                            ChannelsListView(viewModel: .init(nodeInfoClient: .live))
-                        }
-
-                        Button {
-                            isViewPeersPresented = true
-                        } label: {
-                            Text("View Peers")
-                        }
-
+                Section(
+                    header: Text("Wallet")
+                ) {
+                    HStack {
+                        Text("Network")
+                            .fontWeight(.medium)
+                        Spacer()
+                        Text((viewModel.network ?? "No network").capitalized)
+                            .foregroundColor(.secondary)
                     }
-                    .foregroundColor(.primary)
 
-                    Section(header: Text("Danger Zone".uppercased()).foregroundColor(.red)) {
+                    NavigationLink(destination: SeedView(viewModel: .init())) {
+                        Text("Backup")
+                            .fontWeight(.medium)
+                    }
+
+                }
+
+                Section(
+                    header: Text("Lightning node")
+                ) {
+                    HStack {
+                        Text("Node Id")
+                            .fontWeight(.medium)
+                        Spacer()
+                        Text(viewModel.nodeID)
+                            .foregroundColor(.secondary)
+                            .frame(width: 150)
+                            .truncationMode(.middle)
+                            .lineLimit(1)
+                            .padding(.horizontal)
+                        Button {
+                            UIPasteboard.general.string = viewModel.nodeID
+                        } label: {
+                            Image(systemName: "doc.on.doc")
+                        }
+                    }
+
+                    HStack {
+                        Text("Status")
+                            .fontWeight(.medium)
+                        Spacer()
+                        HStack {
+                            Text(viewModel.status?.isRunning ?? false ? "On" : "Off")
+                                .foregroundColor(.secondary)
+                                .padding(.horizontal)
+                            Image(systemName: "circle.fill")
+                                .foregroundColor(
+                                    viewModel.status?.isRunning ?? false ? .green : .red
+                                )
+                        }
+                    }
+
+                    NavigationLink(
+                        destination: ChannelsListView(viewModel: .init(nodeInfoClient: .live))
+                    ) {
+                        Text("Channels")
+                            .fontWeight(.medium)
+                    }
+
+                    NavigationLink(destination: PeersListView(viewModel: .init())) {
+                        Text("Peers")
+                            .fontWeight(.medium)
+                    }
+
+                }
+
+                Section(header: Text("Danger Zone".uppercased()).foregroundColor(.red)) {
+                    /*
                         Button("Show Seed") {
                             showingShowSeedConfirmation = true
                         }
@@ -89,94 +108,82 @@ struct SettingsView: View {
                             Button("Yes", role: .destructive) { isSeedPresented = true }
                             Button("No", role: .cancel) {}
                         }
+                         */
 
-                        Button("Stop Node") {
-                            showingStopNodeConfirmation = true
-                        }
-                        .alert(
-                            "Are you sure you want to stop the node?",
-                            isPresented: $showingStopNodeConfirmation
-                        ) {
-                            Button("Yes", role: .destructive) { viewModel.stop() }
-                            Button("No", role: .cancel) {}
-                        }
-
-                        Button("Reset Preferences") {
-                            showingResetAppConfirmation = true
-                        }
-                        .alert(
-                            "Are you sure you want to reset preferences (and delete the seed)?",
-                            isPresented: $showingResetAppConfirmation
-                        ) {
-                            Button("Yes", role: .destructive) {
-                                viewModel.onboarding()
-                                dismiss()
-                            }
-                            Button("No", role: .cancel) {}
-                        }
-
-                        Button("Delete Seed") {
-                            showingDeleteSeedConfirmation = true
-                        }
-                        .alert(
-                            "Are you sure you want to delete the seed (and reset preferences)?",
-                            isPresented: $showingDeleteSeedConfirmation
-                        ) {
-                            Button("Yes", role: .destructive) {
-                                viewModel.delete()
-                                dismiss()
-                            }
-                            Button("No", role: .cancel) {}
-                        }
+                    Button("Stop Node") {
+                        showingStopNodeConfirmation = true
                     }
-                    .foregroundColor(.primary)
-                }
-                .listRowSeparator(.hidden)
-                .listStyle(.plain)
-                .background(Color.clear)
-                .navigationTitle("Settings")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("Done") {
+                    .alert(
+                        "Are you sure you want to stop the node?",
+                        isPresented: $showingStopNodeConfirmation
+                    ) {
+                        Button("Yes", role: .destructive) { viewModel.stop() }
+                        Button("No", role: .cancel) {}
+                    }
+
+                    Button("Reset Preferences") {
+                        showingResetAppConfirmation = true
+                    }
+                    .alert(
+                        "Are you sure you want to reset preferences (and delete the seed)?",
+                        isPresented: $showingResetAppConfirmation
+                    ) {
+                        Button("Yes", role: .destructive) {
+                            viewModel.onboarding()
                             dismiss()
-                        }.padding()
-                    }
-                }
-                .onAppear {
-                    Task {
-                        viewModel.getNodeID()
-                        viewModel.getNetwork()
-                        viewModel.getEsploraUrl()
-                        viewModel.getColor()
-                        await viewModel.getStatus()
-                    }
-                }
-                .onReceive(viewModel.$nodeIDError) { errorMessage in
-                    showingNodeIDErrorAlert = errorMessage != nil
-                }
-                .alert(isPresented: $showingNodeIDErrorAlert) {
-                    Alert(
-                        title: Text(viewModel.nodeIDError?.title ?? "Unknown"),
-                        message: Text(viewModel.nodeIDError?.detail ?? ""),
-                        dismissButton: .default(Text("OK")) {
-                            viewModel.nodeIDError = nil
                         }
-                    )
-                }
-                .sheet(isPresented: $isSeedPresented) {
-                    SeedView(viewModel: .init())
-                        .presentationDetents([.medium, .large])
-                        .presentationDragIndicator(.visible)
-                }
-                .sheet(
-                    isPresented: $isViewPeersPresented,
-                    onDismiss: {
+                        Button("No", role: .cancel) {}
                     }
-                ) {
-                    PeersListView(viewModel: .init())
-                        .presentationDetents([.medium])
-                }
 
+                    Button("Delete Seed") {
+                        showingDeleteSeedConfirmation = true
+                    }
+                    .alert(
+                        "Are you sure you want to delete the seed (and reset preferences)?",
+                        isPresented: $showingDeleteSeedConfirmation
+                    ) {
+                        Button("Yes", role: .destructive) {
+                            viewModel.delete()
+                            dismiss()
+                        }
+                        Button("No", role: .cancel) {}
+                    }
+                }
+                .foregroundColor(.primary)
+            }
+            .listRowSeparator(.hidden)
+            .listStyle(.plain)
+            .scrollContentBackground(.hidden)
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        dismiss()
+                    }.padding()
+                }
+            }
+            .onAppear {
+                Task {
+                    viewModel.getNodeID()
+                    viewModel.getNetwork()
+                    viewModel.getEsploraUrl()
+                    viewModel.getColor()
+                    await viewModel.getStatus()
+                }
+            }
+            .onReceive(viewModel.$nodeIDError) { errorMessage in
+                showingNodeIDErrorAlert = errorMessage != nil
+            }
+            .alert(isPresented: $showingNodeIDErrorAlert) {
+                Alert(
+                    title: Text(viewModel.nodeIDError?.title ?? "Unknown"),
+                    message: Text(viewModel.nodeIDError?.detail ?? ""),
+                    dismissButton: .default(Text("OK")) {
+                        viewModel.nodeIDError = nil
+                    }
+                )
+            }
         }
 
     }
