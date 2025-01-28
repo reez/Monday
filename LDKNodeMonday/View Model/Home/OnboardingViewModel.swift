a582522912e811f5657807b09e5fb3e3d199071a
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2025-01-28 13:22:56 +0000

    Use NetworkSettingsViewModel for selected network and server

--- LDKNodeMonday/View Model/Home/OnboardingViewModel.swift
+++ LDKNodeMonday/View Model/Home/OnboardingViewModel.swift
@@ -5,12 +5,13 @@
 //  Created by Matthew Ramsden on 1/4/24.
 //
 
+import Combine
 import LDKNode
 import SwiftUI
 
 class OnboardingViewModel: ObservableObject {
     let lightningClient: LightningNodeClient
-    let networkSettingsViewModel: NetworkSettingsViewModel
+    @Published var networkSettingsViewModel: NetworkSettingsViewModel
     @Binding var walletClient: WalletClient
     @Published var onboardingViewError: MondayError?
     @Published var seedPhrase: String = "" {
@@ -19,69 +20,23 @@ class OnboardingViewModel: ObservableObject {
         }
     }
     @Published var seedPhraseArray: [String] = []
-    @Published var selectedNetwork: Network = .signet {
-        didSet {
-            do {
-                guard let server = availableServers(network: walletClient.network).first else {
-                    // This should never happen, but if it does:
-                    fatalError(
-                        "Configuration error: No Esplora servers available for \(selectedNetwork)"
-                    )
-                }
-                self.selectedEsploraServer = server
-                try walletClient.keyClient.saveNetwork(selectedNetwork.description)
-            } catch {
-                DispatchQueue.main.async {
-                    self.onboardingViewError = .init(
-                        title: "Error Selecting Network",
-                        detail: error.localizedDescription
-                    )
-                }
-            }
-        }
-    }
-    @Published var selectedEsploraServer: EsploraServer = EsploraServer.mutiny_signet
-    {
-        didSet {
-            do {
-                try walletClient.keyClient.saveServerURL(selectedEsploraServer.url)
-            } catch {
-                DispatchQueue.main.async {
-                    self.onboardingViewError = .init(
-                        title: "Error Selecting Esplora",
-                        detail: error.localizedDescription
-                    )
-                }
-            }
-        }
-    }
-
-    var buttonColor: Color {
-        switch selectedNetwork {
-        case .bitcoin:
-            return Constants.BitcoinNetworkColor.bitcoin.color
-        case .testnet:
-            return Constants.BitcoinNetworkColor.testnet.color
-        case .signet:
-            return Constants.BitcoinNetworkColor.signet.color
-        case .regtest:
-            return Constants.BitcoinNetworkColor.regtest.color
-        }
-    }
+    private var cancellables = Set<AnyCancellable>()
 
     init(walletClient: Binding<WalletClient>, networkSettingsViewModel: NetworkSettingsViewModel) {
         _walletClient = walletClient
         self.networkSettingsViewModel = networkSettingsViewModel
         self.lightningClient = walletClient.lightningClient.wrappedValue
-        self.selectedNetwork = walletClient.network.wrappedValue
-        self.selectedEsploraServer = walletClient.server.wrappedValue
+
+        networkSettingsViewModel.objectWillChange.sink { [weak self] _ in
+            self?.objectWillChange.send()
+        }.store(in: &cancellables)
     }
 
     func saveSeed() async {
         await walletClient.createWallet(
             seedPhrase: seedPhrase,
-            network: selectedNetwork,
-            server: selectedEsploraServer
+            network: networkSettingsViewModel.selectedNetwork,
+            server: networkSettingsViewModel.selectedEsploraServer
         )
     }
 
