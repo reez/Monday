54e44b128cbe6f062b426f991a3408db7cc51e44
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2025-01-28 14:38:40 +0000

    Further un-refactoring of settingsview

--- LDKNodeMonday/View/Settings/SettingsView.swift
+++ LDKNodeMonday/View/Settings/SettingsView.swift
@@ -19,13 +19,117 @@ struct SettingsView: View {
 
     var body: some View {
         NavigationView {
-            SettingsFormView(
-                viewModel: viewModel,
-                showCheckmark: $showCheckmark,
-                showStopNodeConfirmation: $showStopNodeConfirmation,
-                showDeleteSeedConfirmation: $showDeleteSeedConfirmation,
-                dismiss: dismiss
-            )
+            Form {
+                // Wallet section
+                Section {
+                    NavigationLink(
+                        destination: SeedView(
+                            viewModel: .init(lightningClient: viewModel.lightningClient)
+                        )
+                    ) {
+                        Label("Recovery Phrase", systemImage: "lock")
+                    }
+
+                    NavigationLink(
+                        destination: NetworkSettingsView().environmentObject(
+                            NetworkSettingsViewModel.init(walletClient: $viewModel.walletClient)
+                        )
+                    ) {
+                        Label("Network", systemImage: "network")
+                            .badge((viewModel.network ?? "No network").capitalized)
+                    }
+                } header: {
+                    Text("Wallet").foregroundColor(.primary)
+                }
+
+                // Lightning Node section
+                Section {
+                    HStack {
+                        Label("ID", systemImage: "bolt")
+                        Spacer()
+                        Text(viewModel.nodeID)
+                            .frame(width: 150)
+                            .truncationMode(.middle)
+                            .lineLimit(1)
+                            .foregroundColor(.secondary)
+                        Button {
+                            UIPasteboard.general.string = viewModel.nodeID
+                            showCheckmark = true
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                showCheckmark = false
+                            }
+                        } label: {
+                            Image(systemName: showCheckmark ? "checkmark" : "doc.on.doc")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(showCheckmark ? .secondary : .accentColor)
+                                .accessibilityLabel(showCheckmark ? "Copied" : "Copy node ID")
+                        }
+                    }
+
+                    Label("Status", systemImage: "power")
+                        .badge(viewModel.status?.isRunning ?? false ? "On" : "Off")
+
+                    NavigationLink(
+                        destination: ChannelsListView(
+                            viewModel: .init(
+                                nodeInfoClient: .live,
+                                lightningClient: viewModel.lightningClient
+                            )
+                        )
+                    ) {
+                        Label("Channels", systemImage: "fibrechannel")
+                    }
+
+                    NavigationLink(
+                        destination: PeersListView(
+                            viewModel: .init(lightningClient: viewModel.lightningClient)
+                        )
+                    ) {
+                        Label("Peers", systemImage: "person.line.dotted.person")
+                    }
+                } header: {
+                    Text("Lightning Node").foregroundColor(.primary)
+                }
+
+                // Danger Zone section
+                Section {
+                    Button {
+                        showStopNodeConfirmation = true
+                    } label: {
+                        Text("Stop Node")
+                    }
+                    .foregroundColor(.red)
+                    .alert("Warning!", isPresented: $showStopNodeConfirmation) {
+                        Button("Yes", role: .destructive) { viewModel.stop() }
+                        Button("No", role: .cancel) {}
+                    } message: {
+                        Text("Are you sure you want to stop the node?")
+                    }
+
+                    Button {
+                        showDeleteSeedConfirmation = true
+                    } label: {
+                        Text("Delete Wallet")
+                    }
+                    .foregroundColor(.red)
+                    .alert("Warning!", isPresented: $showDeleteSeedConfirmation) {
+                        Button("Yes", role: .destructive) {
+                            Task {
+                                await viewModel.walletClient.delete()
+                                dismiss()
+                            }
+                        }
+                        Button("No", role: .cancel) {}
+                    } message: {
+                        Text("All funds will be lost.\nAre you sure you want to delete the wallet?")
+                    }
+                } header: {
+                    Text("Danger Zone").foregroundColor(.primary)
+                }
+                .foregroundColor(.primary)
+            }
             .dynamicTypeSize(...DynamicTypeSize.accessibility1)
             .listStyle(.plain)
             .navigationTitle("Settings")
@@ -55,126 +159,6 @@ struct SettingsView: View {
                     }
                 )
             }
-        }
-    }
-}
-
-struct SettingsFormView: View {
-    @ObservedObject var viewModel: SettingsViewModel
-    @Binding var showCheckmark: Bool
-    @Binding var showStopNodeConfirmation: Bool
-    @Binding var showDeleteSeedConfirmation: Bool
-    let dismiss: DismissAction
-
-    var body: some View {
-        Form {
-            // Wallet section
-            Section {
-                NavigationLink(
-                    destination: SeedView(viewModel: .init(lightningClient: viewModel.lightningClient))
-                ) {
-                    Label("Recovery Phrase", systemImage: "lock")
-                }
-
-                NavigationLink(
-                    destination: NetworkSettingsView().environmentObject(
-                        NetworkSettingsViewModel.init(walletClient: $viewModel.walletClient)
-                    )
-                ) {
-                    Label("Network", systemImage: "network")
-                        .badge((viewModel.network ?? "No network").capitalized)
-                }
-            } header: {
-                Text("Wallet").foregroundColor(.primary)
-            }
-            
-            // Lightning Node section
-            Section {
-                HStack {
-                    Label("ID", systemImage: "bolt")
-                    Spacer()
-                    Text(viewModel.nodeID)
-                        .frame(width: 150)
-                        .truncationMode(.middle)
-                        .lineLimit(1)
-                        .foregroundColor(.secondary)
-                    Button {
-                        UIPasteboard.general.string = viewModel.nodeID
-                        showCheckmark = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                            showCheckmark = false
-                        }
-                    } label: {
-                        Image(systemName: showCheckmark ? "checkmark" : "doc.on.doc")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(showCheckmark ? .secondary : .accentColor)
-                            .accessibilityLabel(showCheckmark ? "Copied" : "Copy node ID")
-                    }
-                }
-
-                Label("Status", systemImage: "power")
-                    .badge(viewModel.status?.isRunning ?? false ? "On" : "Off")
-
-                NavigationLink(
-                    destination: ChannelsListView(
-                        viewModel: .init(
-                            nodeInfoClient: .live,
-                            lightningClient: viewModel.lightningClient
-                        )
-                    )
-                ) {
-                    Label("Channels", systemImage: "fibrechannel")
-                }
-
-                NavigationLink(
-                    destination: PeersListView(
-                        viewModel: .init(lightningClient: viewModel.lightningClient)
-                    )
-                ) {
-                    Label("Peers", systemImage: "person.line.dotted.person")
-                }
-            } header: {
-                Text("Lightning Node").foregroundColor(.primary)
-            }
-            
-            // Danger Zone section
-            Section {
-                Button {
-                    showStopNodeConfirmation = true
-                } label: {
-                    Text("Stop Node")
-                }
-                .foregroundColor(.red)
-                .alert("Warning!", isPresented: $showStopNodeConfirmation) {
-                    Button("Yes", role: .destructive) { viewModel.stop() }
-                    Button("No", role: .cancel) {}
-                } message: {
-                    Text("Are you sure you want to stop the node?")
-                }
-
-                Button {
-                    showDeleteSeedConfirmation = true
-                } label: {
-                    Text("Delete Wallet")
-                }
-                .foregroundColor(.red)
-                .alert("Warning!", isPresented: $showDeleteSeedConfirmation) {
-                    Button("Yes", role: .destructive) {
-                        Task {
-                            await viewModel.walletClient.delete()
-                            dismiss()
-                        }
-                    }
-                    Button("No", role: .cancel) {}
-                } message: {
-                    Text("All funds will be lost.\nAre you sure you want to delete the wallet?")
-                }
-            } header: {
-                Text("Danger Zone").foregroundColor(.primary)
-            }
-            .foregroundColor(.primary)
         }
     }
 }
