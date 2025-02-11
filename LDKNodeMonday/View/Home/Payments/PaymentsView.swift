bc489261cd42bf0c8b9996cdd346dcb2aba18d5d
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2025-02-11 15:27:31 +0000

    Refactor PaymentsView

--- LDKNodeMonday/View/Home/Payments/PaymentsView.swift
+++ LDKNodeMonday/View/Home/Payments/PaymentsView.swift
@@ -5,43 +5,27 @@
 //  Created by Matthew Ramsden on 6/20/23.
 //
 
-import SwiftUI
 import LDKNode
+import SwiftUI
 
 struct PaymentsView: View {
     @Binding var transactions: [PaymentDetails]
 
     var body: some View {
-
-        ZStack {
-            Color(uiColor: UIColor.systemBackground)
-
-            VStack {
-
-                HStack {
-                    Text("Payment History")
-                        .font(.title)
-                        .bold()
-                    Spacer()
-                }
-                .padding()
-                .padding(.top, 40.0)
-
+        VStack {
+            if transactions.isEmpty {
                 Spacer()
-
-                if transactions.isEmpty {
-                    Text("No Payments")
-                        .font(.system(.caption, design: .monospaced))
-                        .padding()
-                } else {
-                    PaymentsListView(payments: transactions)
-                }
-
+                Text("No activity yet")
+                    .font(.subheadline)
+                Spacer()
+            } else {
+                PaymentsListView(payments: transactions)
+                    .refreshable {
+                        // TODO: expose getTransactions() from BitcoinViewModel
+                    }
             }
         }
-
     }
-
 }
 
 #if DEBUG
