939ade2e2e5cead4c9061734c1cbedc9c361e70f
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2025-02-25 15:53:19 +0000

    msats / sats as btc extensions

--- LDKNodeMonday/Extensions/UInt64+Extensions.swift
+++ LDKNodeMonday/Extensions/UInt64+Extensions.swift
@@ -16,8 +16,16 @@ extension UInt64 {
         return self >= 1000 ? self / UInt64.msatsPerSat : 0
     }
 
+    var mSatsAsBTC: Double {
+        return Double(self.mSatsAsSats) / 100_000_000
+    }
+
     var satsAsMsats: UInt64 {
         return self * UInt64.msatsPerSat
+    }
+
+    var satsAsBTC: Double {
+        return Double(self) / 100_000_000
     }
 
     func formattedSatsAsBtc(format: BitcoinFormatting? = .truncated) -> String {
