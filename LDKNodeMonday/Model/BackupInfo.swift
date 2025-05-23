7b1f11b2e74e80285efa4c977ae8ef721fa63042
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2025-05-23 15:25:59 +0100

    Add lsp to BackupInfo

--- LDKNodeMonday/Model/BackupInfo.swift
+++ LDKNodeMonday/Model/BackupInfo.swift
@@ -12,17 +12,23 @@ struct BackupInfo: Codable, Equatable {
     var mnemonic: String
     var networkString: String
     var serverURL: String
+    var lspNodeId: String?
 
-    init(mnemonic: String, networkString: String, serverURL: String) {
+    init(mnemonic: String, networkString: String, serverURL: String, lspString: String? = nil) {
         self.mnemonic = mnemonic
         self.networkString = networkString
         self.serverURL = serverURL
+        self.lspNodeId = lspString
     }
 
     static func == (lhs: BackupInfo, rhs: BackupInfo) -> Bool {
-        return lhs.mnemonic == rhs.mnemonic
+        return lhs.mnemonic == rhs.mnemonic &&
+               lhs.networkString == rhs.networkString &&
+               lhs.serverURL == rhs.serverURL &&
+               lhs.lspNodeId == rhs.lspNodeId
     }
 }
+
 
 //#if DEBUG
 let mockBackupInfo = BackupInfo(
