523cb1da0be09d95a9f0366e08e9c65e19829cec
Matthew Ramsden <6657488+reez@users.noreply.github.com>
2025-06-25 14:49:59 -0500

    chore: receive case where capacity+amount is 0

--- LDKNodeMonday/View Model/Home/Receive/ReceiveViewModel.swift
+++ LDKNodeMonday/View Model/Home/Receive/ReceiveViewModel.swift
@@ -34,7 +34,8 @@ class ReceiveViewModel: ObservableObject {
         await MainActor.run {
             self.addressGenerationStatus = .generating
         }
-        let needsJIT = amountSat.satsAsMsats > maxReceiveCapacity()
+        let receiveCapacity = maxReceiveCapacity()
+        let needsJIT = amountSat.satsAsMsats > receiveCapacity || (amountSat == 0 && receiveCapacity == 0)
         if needsJIT {
             // Generate onchain and bolt11Jit, create BIP21 from those
             let addresses = await generateUnifiedWithJIT()
@@ -103,8 +104,12 @@ class ReceiveViewModel: ObservableObject {
         }
 
         // Bolt11
+        let receiveCapacity = maxReceiveCapacity()
+        let needsJIT = amountSat.satsAsMsats > receiveCapacity || (amountSat == 0 && receiveCapacity == 0)
+
+        // Always try to generate bolt11 invoice
+        // The needsJIT flag will handle JIT channel creation when capacity is insufficient
         do {
-            let needsJIT = amountSat.satsAsMsats > maxReceiveCapacity()
             let bolt11Invoice = try await lightningClient.bolt11Payment(
                 amountSat.satsAsMsats,
                 Bolt11InvoiceDescription.direct(description: message),  //message,
